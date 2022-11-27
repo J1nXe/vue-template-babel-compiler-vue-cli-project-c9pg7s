0ebae8654fa73bcac42a62824bf9866e9726d8d7
J1nXe <119157317+J1nXe@users.noreply.github.com>
2022-11-27 19:52:49 +0100

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
-  <h1>{{title}}</h1>
+  <h1>{{ title }}</h1>
+  <input type="text" ref="name">
+  <button @click="handelClick">Click ME</button>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data() {
     return {
-      title: 'my fist Vau app:'
-    }
-  }
-}
+      title: 'my fist Vau app:',
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+    },
+  },
+};
 </script>
 
 <style>
@@ -23,7 +29,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
-h1  {
+h1 {
   border-bottom: 10px;
 }
 </style>
