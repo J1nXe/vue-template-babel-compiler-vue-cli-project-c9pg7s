a9a6313d72679b51f988eefe3bf25d0ca06c67f5
J1nXe <119157317+J1nXe@users.noreply.github.com>
2022-11-27 19:41:32 +0100

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,17 @@
 <template>
-  <div id="app">
-    <h2 v-if="optional?.chaining">
-      Optional Chaining enabled: {{ optional?.chaining }}
-    </h2>
-    <h2>{{ null ?? 'Nullish Coalescing enabled' }}</h2>
-    <h2>{{ BigInt(9007199254740991) }}</h2>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <FunctionalComponent msg="FunctionalComponent valid" />
-    <HelloWorld v-slot:default="slotProps" msg="Welcome to Your Vue.js App">
-      {{ slotProps.dataMsg }}
-    </HelloWorld>
-  </div>
+  <h1>{{title}}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import FunctionalComponent from './components/FunctionalComponent.functional.vue';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-    FunctionalComponent,
-  },
   data() {
     return {
-      optional: {
-        chaining: true,
-      },
-    };
-  },
-};
+      title: 'my fist Vau app:'
+    }
+  }
+}
 </script>
 
 <style>
@@ -41,5 +22,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1  {
+  border-bottom: 10px;
 }
 </style>
